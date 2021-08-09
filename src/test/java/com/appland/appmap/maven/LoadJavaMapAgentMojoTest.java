85902e703d713290cd5e609ac1e174f085da6c70
Kevin Gilpin <kgilpin@gmail.com>
2021-08-09 17:48:17 -0400

    fix: Update AppMap agent code for 1.3.0 API

--- src/test/java/com/appland/appmap/maven/LoadJavaMapAgentMojoTest.java
+++ src/test/java/com/appland/appmap/maven/LoadJavaMapAgentMojoTest.java
@@ -4,24 +4,25 @@ import static org.junit.Assert.assertEquals;
 
 import com.appland.appmap.output.v1.Event;
 
-import com.appland.appmap.record.IRecordingSession;
+import com.appland.appmap.record.RecordingSession;
 import com.appland.appmap.record.Recorder;
+import com.appland.appmap.record.Recording;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
-public class LoadJavaMapAgentMojoTest {
+import java.io.IOException;
+import java.io.StringWriter;
 
+public class LoadJavaMapAgentMojoTest {
     @Before
     public void before() throws Exception {
-        final IRecordingSession.Metadata metadata =
-                new IRecordingSession.Metadata();
-
+        final Recorder.Metadata metadata = new Recorder.Metadata();
         Recorder.getInstance().start(metadata);
     }
 
     @Test
-    public void testAllEventsWritten() {
+    public void testAllEventsWritten() throws IOException {
         final Recorder recorder = Recorder.getInstance();
         final Long threadId = Thread.currentThread().getId();
         final Event[] events = new Event[] {
@@ -34,16 +35,23 @@ public class LoadJavaMapAgentMojoTest {
             final Event event = events[i];
             event
                     .setDefinedClass("SomeClass")
+                    .setEvent("call")
                     .setMethodId("SomeMethod")
                     .setStatic(false)
                     .setLineNumber(315)
                     .setThreadId(threadId);
 
+            System.err.println(recorder);
+            System.err.println(event);
             recorder.add(event);
             assertEquals(event, recorder.getLastEvent());
         }
 
-        final String appmapJson = recorder.stop();
+        final Recording recording = recorder.stop();
+        StringWriter appmapWriter = new StringWriter();
+        recording.readFully(true, appmapWriter);
+        String appmapJson = appmapWriter.toString();
+
         final String expectedJson = "\"thread_id\":" + threadId.toString();
         final int numMatches = StringUtils.countMatches(appmapJson, expectedJson);
         assertEquals(numMatches, events.length);
